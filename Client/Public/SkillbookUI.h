4070d4949770a52f375176f187b90f72b22ea306
seokhoon <znzl1737@naver.com>
2022-08-30 14:47:47 +0900

    UI병합본
    머지할게용~

---
+++ Client/Public/SkillbookUI.h
@@ -0,0 +1,52 @@
+#pragma once
+
+#include "Client_Defines.h"
+#include "GameObject.h"
+
+BEGIN(Engine)
+class CTexture;
+class CRenderer;
+class CTransform;
+class CVIBuffer_Rect;
+END
+
+BEGIN(Client)
+
+class CSkillbookUI final : public CGameObject
+{
+private:
+	CSkillbookUI(LPDIRECT3DDEVICE9 pGraphic_Device);
+	CSkillbookUI(const CSkillbookUI& rhs);
+	virtual ~CSkillbookUI() = default;
+
+public:
+	virtual HRESULT Initialize_Prototype() override;
+	virtual HRESULT Initialize(void* pArg)override;
+	virtual void Tick(_float fTimeDelta)override;
+	virtual void Late_Tick(_float fTimeDelta)override;
+	virtual HRESULT Render() override;
+
+private: /* For.Components */
+	CTexture*				m_pTextureCom = nullptr;
+	CRenderer*				m_pRendererCom = nullptr;
+	CTransform*				m_pTransformCom = nullptr;
+	CVIBuffer_Rect*			m_pVIBufferCom = nullptr;
+
+private:
+	_float4x4				m_ProjMatrix;
+	_float					m_fX, m_fY, m_fSizeX, m_fSizeY;
+	_bool					m_bOnCheck = false;
+	
+
+private:
+	HRESULT SetUp_Components();
+	HRESULT SetUp_RenderState();
+	HRESULT Release_RenderState();
+
+public:
+	static CSkillbookUI* Create(LPDIRECT3DDEVICE9 pGraphic_Device);
+	virtual CGameObject* Clone(void* pArg = nullptr) override;
+	virtual void Free() override;
+};
+
+END
